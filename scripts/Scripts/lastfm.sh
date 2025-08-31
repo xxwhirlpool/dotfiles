458dae02702e5aade27ecbf130510dc4410ffcd0
xxwhirlpool <witnesslachesis@disroot.org>
2025-08-30 20:03:35 -0400

    remove daylight from conky

--- scripts/Scripts/lastfm.sh
+++ scripts/Scripts/lastfm.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# get last.fm API key
+if [ -f ~/.privatevars ]; then                                                                               
+  . ~/.privatevars                                                                                         
+fi
+
 user="springpool"
-LASTFM_APIKEY="x"
 URL="https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$user&api_key=$LASTFM_APIKEY&format=json&limit=1"
 
 artist=$(curl -s ${URL} | jq -r '.recenttracks.track[0].artist."#text"')
