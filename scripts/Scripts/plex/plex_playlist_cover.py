9a90bd772f2e662af5516d1bf00035bb7d2bcd27
xxwhirlpool <witnesslachesis@disroot.org>
2026-04-11 20:05:52 -0400

    download current plex playlist posters

---
+++ scripts/Scripts/plex/plex_playlist_cover.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "plexapi>=4.18.1",
+# ]
+# ///
+
+# USAGE: plex_playlist.py [TOKEN]
+#
+# download all CURRENTLY SELECTED playlist posters/covers
+# posters are named after the playlists
+
+from plexapi.server import PlexServer
+from plexapi.utils import download
+from plexapi.mixins import PlaylistMixins
+import sys
+
+SERVER_URL = "http://192.168.1.219:32400"
+TOKEN = sys.argv[1]
+
+plex = PlexServer(SERVER_URL, TOKEN)
+
+for plist in plex.playlists(playlistType="audio"):
+    list_title = plist.title
+    posters = PlaylistMixins.posters(plist)
+    filtered_posters = list(filter(lambda x: x.selected == True, posters))
+    for po in filtered_posters:
+        po_img = po.thumb
+        po_img_todl = SERVER_URL + po_img
+        download(po_img_todl, TOKEN, list_title + ".png", "posters/") # img to download, token so it can dl, filename, folder to dl to
